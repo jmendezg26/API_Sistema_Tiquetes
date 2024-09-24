65a41555b53eb5353773a91bf55b78ec7dd05133
JoseMariaZunig <Chema1214@gmail.com>
2024-09-24 09:02:11 -0600

    cambios de prueba

--- API_Sistema_Tiquetes/Controllers/WeatherForecastController.cs
+++ API_Sistema_Tiquetes/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace API_Sistema_Tiquetes.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "HOLA PAOLA", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
